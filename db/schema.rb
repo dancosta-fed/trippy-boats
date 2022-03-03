7d7dd00876b4f585548dea66573c8f852e2096ce
Edward Toonela <edward.toonela@icloud.com>
2022-03-03 14:54:33 -0300

    added host and guest review in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_210637) do
+ActiveRecord::Schema.define(version: 2022_03_03_175152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "boats", force: :cascade do |t|
-    t.boolean "available"
+    t.boolean "available", default: true
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "description"
+    t.string "location"
+    t.integer "price"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
@@ -33,6 +36,26 @@ ActiveRecord::Schema.define(version: 2022_03_02_210637) do
     t.datetime "updated_at", precision: 6, null: false
     t.index ["boat_id"], name: "index_bookings_on_boat_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
+
+  create_table "guest_reviews", force: :cascade do |t|
+    t.string "comment"
+    t.integer "star"
+    t.bigint "user_id", null: false
+    t.bigint "boat_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["boat_id"], name: "index_guest_reviews_on_boat_id"
+    t.index ["user_id"], name: "index_guest_reviews_on_user_id"
+  end
+
+  create_table "host_reviews", force: :cascade do |t|
+    t.string "comment"
+    t.integer "star"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_host_reviews_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,4 +75,7 @@ ActiveRecord::Schema.define(version: 2022_03_02_210637) do
   add_foreign_key "boats", "users"
   add_foreign_key "bookings", "boats"
   add_foreign_key "bookings", "users"
+  add_foreign_key "guest_reviews", "boats"
+  add_foreign_key "guest_reviews", "users"
+  add_foreign_key "host_reviews", "users"
 end
