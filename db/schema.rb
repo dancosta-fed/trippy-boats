a392b29b6072662fc02b467822ec39b18045cf10
Edward Toonela <edward.toonela@icloud.com>
2022-03-03 13:54:44 -0300

    Fixed boats table available row by adding a migration for boolean to be default true

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_03_152755) do
+ActiveRecord::Schema.define(version: 2022_03_03_165102) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "boats", force: :cascade do |t|
-    t.boolean "available"
+    t.boolean "available", default: true
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
