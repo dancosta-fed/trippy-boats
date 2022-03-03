69ec2f0a606f8782a58afd432deb9b7d9021c981
Edward Toonela <edward.toonela@icloud.com>
2022-03-03 12:29:25 -0300

    added rows price, description and location to boats in schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_02_210637) do
+ActiveRecord::Schema.define(version: 2022_03_03_152755) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 2022_03_02_210637) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "user_id", null: false
+    t.string "description"
+    t.string "location"
+    t.integer "price"
     t.index ["user_id"], name: "index_boats_on_user_id"
   end
 
