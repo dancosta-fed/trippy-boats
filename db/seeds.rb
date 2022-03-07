dcd56173f38ecaf9f489db8ca30cca2b0271378c
Edward Toonela <edward.toonela@icloud.com>
2022-03-07 16:58:58 -0300

    created seed and added faker gem

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Creating 25 fake users...'
+
+User.create!(fullname: "Fabian Flamengo", email: "fabian@example.com", password: "123456")
+
+25.times do
+  user = User.new(
+    fullname: Faker::Name.name,
+    email: Faker::Internet.email,
+    password: "123456"
+  )
+  user.save!
+end
+
+puts 'Creating 100 fake boats...'
+
+100.times do
+  boat = Boat.new(
+    name: Faker::Name.female_first_name,
+    location: "#{Faker::Address.country}, #{Faker::Address.city}",
+    description: Faker::Movie.quote,
+    price: rand(45..699),
+    user_id: rand(1..25)
+  )
+  boat.save!
+end
+puts 'Finished!'
