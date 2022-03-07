87e63314b8a3dbb0b543bef9ed23c4f65d34fae2
Edward Toonela <edward.toonela@icloud.com>
2022-03-07 12:38:32 -0300

    populated the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'faker'
+
+
+puts "Adding Mr. Fabio..."
+user = User.create!(fullname: 'Fabio', email: "fabio@gmail.com", password: '123456')
+
+puts 'Creating 50 fake users...'
+
+50.times do |id|
+  user = User.new(
+    email:    Faker::Internet.email,
+    fullname: Faker::Name.name,
+    password: "password"
+  )
+  user.save!
+end
+
+puts 'Creating 100 fake boats...'
+100.times do
+  boat = Boat.new(
+    name:    Faker::Name.female_first_name,
+    description: Faker::TvShows::GameOfThrones.quote,
+    location:  "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    price: rand(0..699),
+    user: User.all.sample
+  )
+  boat.save!
+end
+puts "Finished!"
