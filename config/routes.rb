f055f6d2eeb9b4c0ae0a2c7db34c84793c1691e1
Daniel Costa <dancosta.id@gmail.com>
2022-03-08 13:01:46 -0300

    cancel btn is now working

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show]
   resources :boats do
-    resources :bookings, only: [:create, :delete]
+    resources :bookings, only: [:create]
   end
+
+  resources :bookings, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
