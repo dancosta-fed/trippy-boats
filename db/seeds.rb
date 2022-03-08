bcbbbef0647b1b8ba498796020e6f3d2901d0889
Edward Toonela <edward.toonela@icloud.com>
2022-03-08 14:25:16 -0300

    commented out seeds for manual input

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,29 +11,30 @@ require "open-uri"
 
 
 
-puts 'Creating 2 fake users...'
-
-2.times do |i|
-  user = User.new(
-    email: "user@#{i + 1}example.com",
-    fullname: Faker::Name.name,
-    password: "password"
-  )
-  user.save!
-end
-
-puts 'Creating 25 fake boats...'
-25.times do
-  boat = Boat.new(
-    name:    Faker::Name.female_first_name,
-    description: Faker::TvShows::GameOfThrones.quote,
-    location:  "#{Faker::Address.country}, #{Faker::Address.city}",
-    price: rand(21..699),
-    user: User.all.sample,
-    )
-  boat.save!
-end
-
+# puts 'Creating 2 fake users...'
+#
+# 2.times do |i|
+#   user = User.new(
+#     email: "user@#{i + 1}example.com",
+#     fullname: Faker::Name.name,
+#     password: "password"
+#   )
+#   user.save!
+# end
+#
+# puts 'Creating 6 fake boats...'
+# 6.times do
+#   boat = Boat.new(
+#     name: Faker::Name.female_first_name,
+#     description: Faker::TvShows::GameOfThrones.quote,
+#     location:  "#{Faker::Address.country}, #{Faker::Address.city}",
+#     price: rand(21..699),
+#     user: User.all.sample,
+#     photo: "app/assets/images/android-chrome-512x512.png"
+#     )
+#   boat.save!
+# end
+#
 puts "Adding Mr. Fabio..."
 User.create!(fullname: 'Fabio', email: "fabio@gmail.com", password: '123456')
 
